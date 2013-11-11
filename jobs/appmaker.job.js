51ce9eed292c7a2dc4ab2245652c5d0d633476f8
David Ascher <david.ascher@gmail.com>
2013-11-11 00:56:29 -0800

    namespace change

--- jobs/appmaker.job.js
+++ jobs/appmaker.job.js
@@ -3,7 +3,7 @@ var app_count = 0;
 
 function updateAppCount() {
 	var last_app_count = app_count;
-	var counturl = "http://162.243.75.232/render?format=json&target=summarize(appmaker_counts.app_published,'10year')"
+	var counturl = "http://162.243.75.232/render?format=json&target=summarize(stats_counts.flathead.app_published,'10year')"
 	request.get({url:counturl, json:true}, function (e, r, data) {
 		// console.log("got appcount of", data[0]['datapoints'][0][0]);
 		send_event('app_count', {current: data[0]['datapoints'][0][0],
