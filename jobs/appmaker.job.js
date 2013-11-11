a82cea9246b41563bf94d2376823d8ca54b9892f
David Ascher <david.ascher@gmail.com>
2013-11-11 00:02:00 -0800

    take out github token

--- jobs/appmaker.job.js
+++ jobs/appmaker.job.js
@@ -1,12 +1,17 @@
 var request = require('request');
 var app_count = 0;
 
-setInterval(function() {
+function updateAppCount() {
 	var last_app_count = app_count;
 	var counturl = "http://162.243.75.232/render?format=json&target=summarize(stats_counts.appmaker.app_published,'10year')"
 	request.get({url:counturl, json:true}, function (e, r, data) {
-		console.log("got appcount of", data[0]['datapoints'][0][0]);
+		// console.log("got appcount of", data[0]['datapoints'][0][0]);
 		send_event('app_count', {current: data[0]['datapoints'][0][0],
 			last: last_app_count});
 	})
-}, 20 * 1000);
+}
+
+updateAppCount();
+setInterval(function() {
+	updateAppCount();
+}, 5 * 60 * 1000); // 5 minutes
--- jobs/github.job.js
+++ jobs/github.job.js
@@ -1,57 +1,42 @@
+var second = 1000;
+var minute = 60 * second;
+var hour = 60 * minute;
+
 var github = require('octonode');
-var client = github.client('9e3b11400b3381b72522e477e41f838012492b16');
-console.log('client', client);
-var ghme = client.me()
-console.log('ghme', ghme);
-// var org = octonode.Organization('mozilla');
+var token = process.env['GITHUB_TOKEN'] || '';
+console.log("TOKEN =", token);
+var client = github.client(token);
+
+var ghme = client.me();
 var repo = client.repo('mozilla/appmaker');
-console.log('repo', repo);
 
-var current_issues = 0;
-var current_pulls = 0;
+var contributors = 0;
+var pulls = 0;
+var forks = 0;
+var open_issues = 0;
+var watchers = 0;
 
-
-// function getAllIssues(url, callback) {
-//     repo.issues(page, function(err, data) {
-//         console.log("called page", page, "got data", data);
-//         if (data
-//     })
-
-//     request.get({
-//         url: url,
-//         json: true
-//     }, function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             _.each(body.posts.data, function (post) {
-//                 User.posts.push(post); //push some result
-//             });
-//             if (body.pagination.next) { // if set, this is the next URL to query
-//                 getFacebookData(body.pagination.next, callback);
-//             } else {
-//                 callback(); //Call when we are finished
-//             }
-//         } else {
-//             console.log(error);
-//             throw error;
-//         }
-
-//     });
-// }
+function updateGithubMetrics() {
+    var last_pulls = pulls;
+    var last_watchers = watchers;
+    var last_forks = forks;
+    var last_contributors = contributors;
+    var last_open_issues = open_issues;
+    repo.contributors(function(err, data) {
+        contributors = data.length;
+        send_event('contributors', {current: contributors, last: last_contributors});
+    });
+    repo.info(function(err, data) {
+        watchers = data['watchers'];
+        open_issues = data['open_issues'];
+        forks = data['forks'];
+        send_event('watchers', {current: watchers, last: last_watchers});
+        send_event('forks', {current: forks, last: last_forks});
+        send_event('issues', {current: open_issues, last: last_open_issues});
+    });
+}
+updateGithubMetrics();
 
 setInterval(function() {
-    var last_pulls = current_pulls;
-    var last_issues = current_issues;
-    // var current_pulls = ;
-    // var current_issues = ;
-    console.log('doing github calls');
-    repo.issues(1, function(err, data) {
-        console.log("got cb", err, data);
-        current_issues = data.length
-        console.log(data[0]);
-        console.log(data[1]);
-        send_event('issues', {current: current_issues, last: last_issues});
-    })
-    // send_event('contributors', {value: contribs}); // never goes down
-    // send_event('pulls', {current: current_pulls, last: last_pulls});
-
-}, 200 * 1000);
+    updateGithubMetrics();
+}, hour);
