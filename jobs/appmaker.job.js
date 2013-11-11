c81bc40fb24449699cbdd8f07863d7501db8948c
David Ascher <david.ascher@gmail.com>
2013-11-10 21:36:34 -0800

    got the right path to the data

--- jobs/appmaker.job.js
+++ jobs/appmaker.job.js
@@ -5,11 +5,8 @@ setInterval(function() {
 	var last_app_count = app_count;
 	var counturl = "http://162.243.75.232/render?format=json&target=summarize(stats_counts.appmaker.app_published,'10year')"
 	request.get({url:counturl, json:true}, function (e, r, data) {
-		console.log(data);
-		var x = JSON.parse(data);
-		console.log('parsed', x);
-		console.log(x['datapoints'][0][0]);
-		send_event('app_count', {current: data['datapoints'][0][0],
+		console.log("got appcount of", data[0]['datapoints'][0][0]);
+		send_event('app_count', {current: data[0]['datapoints'][0][0],
 			last: last_app_count});
 	})
 }, 20 * 1000);
