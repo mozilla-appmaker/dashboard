6a2c163a79dff3ff9464cc725037f133947ca02e
David Ascher <david.ascher@gmail.com>
2013-11-27 16:17:40 -0800

    include apps before metrics were implemented

--- jobs/appmaker.job.js
+++ jobs/appmaker.job.js
@@ -1,12 +1,13 @@
 var request = require('request');
-var app_count = 0;
+var BASE = 12699;
+var app_count = BASE;
 
 function updateAppCount() {
 	var last_app_count = app_count;
 	var counturl = "http://162.243.75.232/render?format=json&target=summarize(stats_counts.appmaker.live.app_published,'10year')";
 	request.get({url:counturl, json:true}, function (e, r, data) {
 		// console.log("got appcount of", data[0]['datapoints'][0][0]);
-		send_event('app_count', {current: data[0]['datapoints'][0][0],
+		send_event('app_count', {current: BASE+data[0]['datapoints'][0][0],
 			last: last_app_count});
 	})
 }
